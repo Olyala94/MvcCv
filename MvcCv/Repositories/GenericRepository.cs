b679d7f8053fcd5329f8eec8f47790b2f9d0d30e
Olya <119108499+Olyala94@users.noreply.github.com>
2023-06-22 07:58:19 +0300

    Bölüm 5: Repository Design Pattern #24 - Repository Design Pattern #25 - Repository'lerin Tanımlanması #26 - Deneyimlerin Listelenmesi

---
+++ MvcCv/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using MvcCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MvcCv.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        DbCvEntities db = new DbCvEntities();   
+
+        public List<T> List() 
+        {
+          return db.Set<T>().ToList();
+        }
+
+        public void TAdd(T p)
+        {
+            db.Set<T>().Add(p); 
+            db.SaveChanges();
+        }
+
+        public void TDelete(T p)
+        {
+            db.Set<T>().Remove(p);  
+            db.SaveChanges();   
+        }
+
+        public T TGet(int id) //"int" - olduğu için ve "T" türünde olduğu için geriye bir "return" ifadesi olması gerekiyor!
+        {
+            return db.Set<T>().Find(id);  //id - ye göre bul
+        }
+
+        public void TUpdate(T p)
+        {
+            db.SaveChanges(); //"TUpdate" Değerini direk olarak değişikleri kayd et... 
+        }
+    }
+}
