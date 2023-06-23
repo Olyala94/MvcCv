02dabd70cc8ad30db28c6e1cf1fb609401172f5b
Olya <119108499+Olyala94@users.noreply.github.com>
2023-06-23 19:25:30 +0300

    Listed on Talents Repository and Update Model From Database

--- MvcCv/Controllers/YetenekController.cs
+++ MvcCv/Controllers/YetenekController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+    
 namespace MvcCv.Controllers
 {
     public class YetenekController : Controller
     {
         // GET: Yetenek
+        //DbCvEntities db = new DbCvEntities();
+        GenericRepository<TblYetenekler> repo = new GenericRepository<TblYetenekler>();
         public ActionResult Index()
         {
-            return View();
+            var yetenekler = repo.List();
+            return View(yetenekler);
         }
     }
 }
